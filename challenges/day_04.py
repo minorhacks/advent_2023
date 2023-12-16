c6e0d07d57a9f74018993d3f091a9c6d650416a6
Scott Minor <minor@minorhacks.com>
2023-12-16 14:55:17 -0500

    day_04: Finished part 2

--- challenges/day_04.py
+++ challenges/day_04.py
@@ -1,5 +1,6 @@
 import dataclasses
 import io
+import logging as log
 import re
 
 _NUMS_AND_PIPE_RE = re.compile(r"\d+|\|")
@@ -7,6 +8,7 @@ _NUMS_AND_PIPE_RE = re.compile(r"\d+|\|")
 
 @dataclasses.dataclass
 class Card:
+    card_num: int
     score: int
     matching_count: int
     copies: int
@@ -26,7 +28,7 @@ class Card:
         if (matching_count := len(selected_nums.intersection(winning_nums))) > 0:
             score = 2 ** (matching_count - 1)
 
-        return cls(score, matching_count, 1)
+        return cls(card_num, score, matching_count, 1)
 
 
 def part_1(input: io.TextIOBase) -> str:
@@ -37,4 +39,19 @@ def part_1(input: io.TextIOBase) -> str:
 
 
 def part_2(input: io.TextIOBase) -> str:
-    pass
+    cards: dict[int, Card] = {}
+    while (line := input.readline()) != "":
+        c = Card.from_str(line)
+        cards[c.card_num] = c
+
+    for card_num in sorted(cards.keys()):
+        for i in range(cards[card_num].matching_count):
+            cards[card_num + i + 1].copies += cards[card_num].copies
+            log.debug(
+                "card %d increments card %d copies to %d",
+                card_num,
+                card_num + i + 1,
+                cards[card_num + i + 1].copies,
+            )
+
+    return sum(c.copies for c in cards.values())
--- challenges/day_04_test.py
+++ challenges/day_04_test.py
@@ -5,8 +5,8 @@ from absl.testing import absltest
 
 from challenges import day_04
 
-class TestDay04(absltest.TestCase):
 
+class TestDay04(absltest.TestCase):
     def test_part_1(self):
         sample = textwrap.dedent(
             """
@@ -19,6 +19,20 @@ class TestDay04(absltest.TestCase):
             """
         ).strip()
         self.assertEqual(13, day_04.part_1(io.StringIO(sample)))
+
+    def test_part_1(self):
+        sample = textwrap.dedent(
+            """
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+            """
+        ).strip()
+        self.assertEqual(30, day_04.part_2(io.StringIO(sample)))
+
 
 if __name__ == "__main__":
     absltest.main()
